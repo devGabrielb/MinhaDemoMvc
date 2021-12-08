69fa819a707c67e086da1aff4de50d740107ec12
devGabrielb <dev.gabrielb1@gmail.com>
2021-12-07 23:19:00 -0300

    project init

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMvc.Models;
+
+namespace MinhaDemoMvc.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                ///
+            }
+            return View(filme);
+        }
+    }
+}
